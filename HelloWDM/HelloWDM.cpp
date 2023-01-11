e3933cf0dd41e3df6b374d7eb1e98bf3dfb93b01
Liucheng Zhu <13927181971@163.com>
2023-01-11 17:41:24 +0800

    commit 1.0.2

--- HelloWDM/HelloWDM.cpp
+++ HelloWDM/HelloWDM.cpp
@@ -179,36 +179,74 @@ NTSTATUS HelloWDMPnp(IN PDEVICE_OBJECT fdo, IN PIRP Irp)
 	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
 	static NTSTATUS (*fcntab[])(PDEVICE_EXTENSION pdx, PIRP Irp) =
 	{
-		DefaultPnpHandler,	// IRP_MN_START_DEVICE
-		DefaultPnpHandler,	// IRP_MN_QUERY_REMOVE_DEVICE
-		HandleRemoveDevice, // IRP_MN_REMOVE_DEVICE
-		DefaultPnpHandler,	// IRP_MN_CANCEL_REMOVE_DEVICE
-		DefaultPnpHandler,	// IRP_MN_STOP_DEVICE
-		DefaultPnpHandler,	// IRP_MN_QUERY_STOP_DEVICE
-		DefaultPnpHandler,	// IRP_MN_CANCEL_STOP_DEVICE
-		DefaultPnpHandler,	// IRP_MN_QUERY_DEVICE_RELATIONS
-		DefaultPnpHandler,	// IRP_MN_QUERY_INTERFACE
-		DefaultPnpHandler,	// IRP_MN_QUERY_CAPABILITIES
-		DefaultPnpHandler,	// IRP_MN_QUERY_RESOURCES
-		DefaultPnpHandler,	// IRP_MN_QUERY_RESOURCES_REQUIREMENTS
-		DefaultPnpHandler,	// IRP_MN_QUERY_DEVICE_EXIT
-		DefaultPnpHandler,	// IRP_MN_FILTER_RESOURCE_REQUIREMENTS
-		DefaultPnpHandler,	// 
-		DefaultPnpHandler,	// IRP_MN_READ_CONFIG
-		DefaultPnpHandler,	// IRP_MN_WRITE_CONFIG
-		DefaultPnpHandler,	// IRP_MN_EJECT
-		DefaultPnpHandler,	// IRP_MN_SET_LOCK
-		DefaultPnpHandler,	// IRP_MN_QUERY_ID
-		DefaultPnpHandler,	// IRP_MN_QUERY_PNP_DEVICE_STATE
-		DefaultPnpHandler,	// IRP_MN_QUERY_BUS_INFORMATION
-		DefaultPnpHandler,	// IRP_MN_DEVICE_USAGE_NOTIFICATION
-		DefaultPnpHandler,	// IRP_MN_SURPRISE_REMOVAL
+		DefaultPnpHandler,		// IRP_MN_START_DEVICE
+		DefaultPnpHandler,		// IRP_MN_QUERY_REMOVE_DEVICE
+		HandleRemoveDevice,		// IRP_MN_REMOVE_DEVICE
+		DefaultPnpHandler,		// IRP_MN_CANCEL_REMOVE_DEVICE
+		DefaultPnpHandler,		// IRP_MN_STOP_DEVICE
+		DefaultPnpHandler,		// IRP_MN_QUERY_STOP_DEVICE
+		DefaultPnpHandler,		// IRP_MN_CANCEL_STOP_DEVICE
+		DefaultPnpHandler,		// IRP_MN_QUERY_DEVICE_RELATIONS
+		DefaultPnpHandler,		// IRP_MN_QUERY_INTERFACE
+		DefaultPnpHandler,		// IRP_MN_QUERY_CAPABILITIES
+		DefaultPnpHandler,		// IRP_MN_QUERY_RESOURCES
+		DefaultPnpHandler,		// IRP_MN_QUERY_RESOURCE_REQUIREMENTS
+		DefaultPnpHandler,		// IRP_MN_QUERY_DEVICE_TEXT
+		DefaultPnpHandler,		// IRP_MN_FILTER_RESOURCE_REQUIREMENTS
+		DefaultPnpHandler,		// 
+		DefaultPnpHandler,		// IRP_MN_READ_CONFIG
+		DefaultPnpHandler,		// IRP_MN_WRITE_CONFIG
+		DefaultPnpHandler,		// IRP_MN_EJECT
+		DefaultPnpHandler,		// IRP_MN_SET_LOCK
+		DefaultPnpHandler,		// IRP_MN_QUERY_ID
+		DefaultPnpHandler,		// IRP_MN_QUERY_PNP_DEVICE_STATE
+		DefaultPnpHandler,		// IRP_MN_QUERY_BUS_INFORMATION
+		DefaultPnpHandler,		// IRP_MN_DEVICE_USAGE_NOTIFICATION
+		DefaultPnpHandler,		// IRP_MN_SURPRISE_REMOVAL
 	};
 
-	KdPrint(("Leave HelloWDMPnp\n"));
-	return STATUS_SUCCESS;
-}
+	ULONG fcn = stack->MinorFunction;
+	if (fcn >= arraysize(fcntab))
+	{
+		// unknown function
+		status = DefaultPnpHandler(pdx, Irp); // some function we don't know about
+		return status;
+	}
+#if DBG
+	static char* fcnname[] =
+	{
+		"IRP_MN_START_DEVICE",
+		"IRP_MN_QUERY_REMOVE_DEVICE",
+		"IRP_MN_REMOVE_DEVICE",
+		"IRP_MN_CANCEL_REMOVE_DEVICE",
+		"IRP_MN_STOP_DEVICE",
+		"IRP_MN_QUERY_STOP_DEVICE",
+		"IRP_MN_CANCEL_STOP_DEVICE",
+		"IRP_MN_QUERY_DEVICE_RELATIONS",
+		"IRP_MN_QUERY_INTERFACE",
+		"IRP_MN_QUERY_CAPABILITIES",
+		"IRP_MN_QUERY_RESOURCES",
+		"IRP_MN_QUERY_RESOURCE_REQUIREMENTS",
+		"IRP_MN_QUERY_DEVICE_TEXT",
+		"IRP_MN_FILTER_RESOURCE_REQUIREMENTS",
+		"",
+		"IRP_MN_READ_CONFIG",
+		"IRP_MN_WRITE_CONFIG",
+		"IRP_MN_EJECT",
+		"IRP_MN_SET_LOCK",
+		"IRP_MN_QUERY_ID",
+		"IRP_MN_QUERY_PNP_DEVICE_STATE",
+		"IRP_MN_QUERY_BUS_INFORMATION",
+		"IRP_MN_DEVICE_USAGE_NOTIFICATION",
+		"IRP_MN_SURPRISE_REMOVAL",
+	};
 
+	KdPrint(("PNP Request (%s)\n", fcnname[fcn]));
+#endif
+	status = (*fcntab[fcn])(pdx, Irp);
+	KdPrint(("Leave HelloWDMPnp\n"));
+	return status;
+}
 
 /************************************************************************************************
 * º¯ÊýÃû³Æ£ºHelloWDMUnload
